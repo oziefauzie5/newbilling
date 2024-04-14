52abd1250ca72dd1cabd1f99695fe0e2d3bc6604
oziefauzie5 <oziefauzie31@gmail.com>
2024-04-14 16:54:59 +0700

    update SALES baru

--- app/Http/Controllers/Sales/SalesController.php
+++ app/Http/Controllers/Sales/SalesController.php
@@ -52,7 +52,8 @@ class SalesController extends Controller
     {
         $data['input_data'] = InputData::where('input_status', 0)->count();
         $data['registrasi'] = Registrasi::where('reg_progres', 0)->count();
-        $data['data_pemasangan'] = InputData::where('input_status', 0)->get();
+        $data['data_pemasangan'] = InputData::where('input_status', '0')
+            ->orderBy('input_tgl', 'DESC')->get();
         return view('sales/list_registrasi', $data);
     }
 
