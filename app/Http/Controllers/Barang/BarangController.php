43298a0c3cf8fe1a3189ad0dab1110b163cbf31a
oziefauzie5 <oziefauzie31@gmail.com>
2024-08-05 21:07:49 +0700

    update 5 Agustus 2024 16

--- app/Http/Controllers/Barang/BarangController.php
+++ app/Http/Controllers/Barang/BarangController.php
@@ -109,7 +109,7 @@ class BarangController extends Controller
             });
 
         if ($data['status'])
-            $query->where('subbarang_status', 'like', '%' . $data['status'] . '%');
+            $query->where('subbarang_status.id', '=', $data['status']);
 
 
         $data['SubBarang'] = $query->paginate(20);
