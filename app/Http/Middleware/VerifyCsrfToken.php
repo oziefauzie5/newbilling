83c257a2d565d26810dadbdd8625d37a6149cd2b
oziefauzie5 <oziefauzie31@gmail.com>
2025-05-20 20:38:58 -0700

    update 20-05 2

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        env('LINK_APK').'/callback'
+        'env("LINK_APK")/callback'
     ];
 }
