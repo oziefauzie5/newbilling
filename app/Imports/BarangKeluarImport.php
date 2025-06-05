fb805ccfefbe5aef0eb837e38f9cc827e936ed6c
oziefauzie5 <oziefauzie31@gmail.com>
2025-06-04 23:35:17 -0700

    update 05-06 13

--- app/Imports/BarangKeluarImport.php
+++ app/Imports/BarangKeluarImport.php
@@ -3,15 +3,39 @@
 namespace App\Imports;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Session;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\ToModel;
 
-class BarangKeluarImport implements ToCollection
+class BarangKeluarImport implements ToModel
+
 {
     /**
     * @param Collection $collection
     */
-    public function collection(Collection $collection)
+   public function model(array $row)
     {
-        //
+        return new BarangKeluarImport([
+            'corporate_id' =>Session::get('corp_id'),
+            'bk_jenis_laporan' => $row[0],
+            'bk_id_barang' => $row[1],
+            'bk_idpel' => $row[2],
+            'bk_kategori' => $row[3],
+            'bk_harga' => $row[4],
+            'bk_before' => $row[5],
+            'bk_after' => $row[6],
+            'bk_terpakai' => $row[7],
+            'bk_jumlah' => $row[8],
+            'bk_keperluan' => $row[9],
+            'bk_file_bukti' => $row[10],
+            'bk_nama_pengguna' => $row[11],
+            'bk_waktu_keluar' => $row[12],
+            'bk_admin_input' => $row[13],
+            'bk_penerima' => $row[14],
+            'bk_status' => $row[15],
+            'bk_keterangan' => $row[16],
+        ]);
     }
 }
+
+
