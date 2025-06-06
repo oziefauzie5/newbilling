a99e0ff1ecf8bf74e1f1f7b05de221eb3151e73a
oziefauzie5 <oziefauzie31@gmail.com>
2025-06-06 08:38:15 -0700

    update 06-06 1

--- app/Http/Controllers/Pelanggan/PelangganController.php
+++ app/Http/Controllers/Pelanggan/PelangganController.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Session;
 
 class PelangganController extends Controller
 {
+    public function maintenance()
+    {
+       
+        return view('client/maintenance');
+    }
     public function index()
     {
         $idpel = Auth::guard('pelanggan')->user()->id;
